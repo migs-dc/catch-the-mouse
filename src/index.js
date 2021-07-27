2356be73bc60baf2680355a02e08784824fd74c9
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-07-27 02:31:49 -0400

    add just from platform mechanic

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const height = 500;
   const catHeight = 40;
   const catWidth = 60;
-  const tableHeight = 20;
+  const tableHeight = 200;
   const tableWidth = 200;
+  const floorHeight = 20;
+  let minY = floorHeight;
   
   context.canvas.width = width;
   context.canvas.height = height;
@@ -15,23 +17,24 @@ document.addEventListener('DOMContentLoaded', () => {
     height: catHeight,
     width: catWidth,
     x: 375,
-    y: height - 20 - catHeight,
+    y: height - floorHeight - catHeight,
     xVelocity: 0,
     yVelocity: 0,
-    jumping: false
+    jumping: false,
+    onPlatform: false
   }
-
+  
+  table = {
+    height: tableHeight,
+    width: tableWidth,
+    x: 400,
+    y: height - tableHeight
+  }
+  
   catMove = {
     left: false,
     right: false,
     jump: false
-  }
-
-  table = {
-    height: tableHeight,
-    width: tableWidth,
-    x: 500,
-    y: height - 200
   }
 
   function keyListener(e) {
@@ -53,6 +56,8 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function game() {
+    console.log(cat.y)
+
     if (catMove.jump && !cat.jumping) {
       cat.yVelocity -= Math.abs(cat.xVelocity);
       cat.yVelocity -= 22;
@@ -67,10 +72,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     cat.x += cat.xVelocity;
     cat.xVelocity *= 0.8; // friction
-
-    cat.yVelocity += 2; // gravity
+    
+    cat.yVelocity += 2; 
     cat.y += cat.yVelocity;
-    console.log(cat.y)
 
     if (cat.x <= 0){
       catMove.left = false;
@@ -81,13 +85,33 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     // can't pass through the floor when jumping
-    if (cat.y >= height - 20 - catHeight){
+    if (cat.y >= height - minY - catHeight){
       cat.yVelocity = 0;
       cat.jumping = false;
-      cat.y = height - 20 - catHeight;
+      cat.y = height - minY - cat.height;
     }
 
-    // // screen pass through
+    // land on table
+    if(cat.y <= height - table.height - cat.height){
+      if (cat.x + cat.width > table.x && cat.x < table.x + table.width){    
+        // console.log("in")
+        // cat.yVelocity = 0;
+        // cat.jumping = false;
+        // cat.onPlatform = true;
+        minY = table.height;
+
+        // cat.y = height - table.height - cat.height;
+      } else {
+        // console.log("out")
+        // cat.onPlatform = false;
+        minY = floorHeight;
+      }
+    }
+
+    if(!cat.onPlatform){ 
+    }
+
+    // // pass through sides
     // if (cat.x < -cat.width) {
     //   cat.x = width;
     // } else if (cat.x > width) {
@@ -97,14 +121,14 @@ document.addEventListener('DOMContentLoaded', () => {
     context.fillStyle = "#202020"; // canvas
     context.fillRect(0, 0, 750, 500);// x, y, width, height
     
+    context.fillStyle = "#331800"; // table
+    context.fillRect(table.x, table.y, table.width, table.height);
+    
     context.fillStyle = "#BB814C"; // cat
     context.fillRect(cat.x, cat.y, cat.width, cat.height);
-
-    context.fillStyle = "#654321"; // table
-    context.fillRect(table.x, table.y, table.width, table.height);
-  
+    
     context.fillStyle = "#654321"; // floor
-    context.fillRect(0, 480, 750, 20);
+    context.fillRect(0, 480, width, floorHeight);
 
     window.requestAnimationFrame(game);
   }
