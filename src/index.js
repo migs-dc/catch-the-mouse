2fc95830a44ff6832239f34647e84db6ed13e3ef
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-07-27 03:12:07 -0400

    add 3rd level platform and check if jump and drop work

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const catHeight = 40;
   const catWidth = 60;
   const tableHeight = 200;
-  const tableWidth = 200;
+  const tableWidth = 300;
+  const shelfHeight = 350;
+  const shelfWidth = 200;
+
   const floorHeight = 20;
   let minY = floorHeight;
   
@@ -23,18 +26,26 @@ document.addEventListener('DOMContentLoaded', () => {
     jumping: false,
     onPlatform: false
   }
+
+  let shelf = {
+    height: shelfHeight,
+    width: shelfWidth,
+    x: 300,
+    y: height - shelfHeight
+  }
   
-  table = {
+  let table = {
     height: tableHeight,
     width: tableWidth,
     x: 400,
     y: height - tableHeight
   }
   
-  catMove = {
+  let catMove = {
     left: false,
     right: false,
-    jump: false
+    jump: false,
+    drop: false
   }
 
   function keyListener(e) {
@@ -52,12 +63,13 @@ document.addEventListener('DOMContentLoaded', () => {
       case 38:
         catMove.jump = moving;
         break;
+      case 40:
+        catMove.drop = moving;
+        break;
     }
   }
 
   function game() {
-    console.log(cat.y)
-
     if (catMove.jump && !cat.jumping) {
       cat.yVelocity -= Math.abs(cat.xVelocity);
       cat.yVelocity -= 22;
@@ -76,15 +88,12 @@ document.addEventListener('DOMContentLoaded', () => {
     cat.yVelocity += 2; 
     cat.y += cat.yVelocity;
 
-    if (cat.x <= 0){
-      catMove.left = false;
-      cat.xVelocity = 0;
-    } else if (cat.x >= width - cat.width){
-      catMove.right = false;
-      cat.xVelocity = 0;
+    if(catMove.drop){
+      console.log("in")
+      minY = floorHeight;
     }
 
-    // can't pass through the floor when jumping
+    // can't pass through the floor/platform when jumping
     if (cat.y >= height - minY - catHeight){
       cat.yVelocity = 0;
       cat.jumping = false;
@@ -92,34 +101,45 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     // land on table
-    if(cat.y <= height - table.height - cat.height){
+    if(cat.y <= table.y - cat.height){
       if (cat.x + cat.width > table.x && cat.x < table.x + table.width){    
-        // console.log("in")
-        // cat.yVelocity = 0;
-        // cat.jumping = false;
-        // cat.onPlatform = true;
+        // console.log(cat.y, table.height)
         minY = table.height;
-
-        // cat.y = height - table.height - cat.height;
       } else {
-        // console.log("out")
-        // cat.onPlatform = false;
         minY = floorHeight;
       }
     }
 
-    if(!cat.onPlatform){ 
+    if(cat.y <= shelf.y - cat.height){
+      if (cat.x + cat.width > shelf.x && cat.x < shelf.x + shelf.width){    
+        console.log(cat.y, shelf.height)
+        minY = shelf.height;
+      } else {
+        minY = floorHeight;
+      }
     }
 
-    // // pass through sides
-    // if (cat.x < -cat.width) {
-    //   cat.x = width;
-    // } else if (cat.x > width) {
-    //   cat.x = -32;
+    // can't go passed the left and right borders
+    // if (cat.x <= 0){
+    //   catMove.left = false;
+    //   cat.xVelocity = 0;
+    // } else if (cat.x >= width - cat.width){
+    //   catMove.right = false;
+    //   cat.xVelocity = 0;
     // }
+
+    // pass through sides
+    if (cat.x < -cat.width) {
+      cat.x = width;
+    } else if (cat.x > width) {
+      cat.x = -cat.width;
+    }
     
     context.fillStyle = "#202020"; // canvas
     context.fillRect(0, 0, 750, 500);// x, y, width, height
+
+    context.fillStyle = "#1B0000"; // shelf
+    context.fillRect(shelf.x, shelf.y, shelf.width, shelf.height);
     
     context.fillStyle = "#331800"; // table
     context.fillRect(table.x, table.y, table.width, table.height);
