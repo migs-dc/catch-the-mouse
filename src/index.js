a3cd36e3d9b41d49504034f964c43c089d97024d
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-08-19 21:01:25 -0400

    shortening the width of the cat's hit box but not the picture

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const width = 1500;
   const height = 700;
   const catHeight = 75;
-  const catWidth = 100;
+  const catWidth = 70;
   const mouseHeight = 25;
   const mouseWidth = 45;
   
